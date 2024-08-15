e12b5bf879bd9c71cb3e08ee5e2a7efa28226fee
benmash33 <bxm5502@psu.edu>
2024-08-15 15:01:06 -0400

    Initial commit for GUI files

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BetMachine from './BetMachine';
+
+function App() {
+  return (
+    <div className="App">
+      <BetMachine />
+    </div>
+  );
+}
+
+export default App;
